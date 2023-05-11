70d3a35e205ff752c70306c5cff98ebbee9cb732
lienputnina <liene.putnina20@gmail.com>
2023-05-11 16:25:06 +0300

    feat: configured posting loading states, added toast alerts

--- src/components/CreatePostWizard.tsx
+++ src/components/CreatePostWizard.tsx
@@ -1,28 +1,37 @@
-import type {FC} from 'react';
+import type { FC } from 'react';
 import { useState } from 'react';
 
 import { useUser } from '@clerk/nextjs';
 import Image from 'next/image';
 import { api } from '~/utils/api';
-
+import { toast } from 'react-hot-toast';
+import { LoadingSpinner } from './Loading/LoadingSpinner';
 
 const CreatePostWizard: FC = () => {
-
   const { user } = useUser();
 
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState('');
 
-  const ctx = api.useContext()
+  const ctx = api.useContext();
 
-  const {mutate, isLoading: isPosting} = api.posts.create.useMutation({
+  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
     onSuccess: () => {
-      setInput("");
-      void ctx.posts.getAll.invalidate()
+      setInput('');
+      void ctx.posts.getAll.invalidate();
+    },
+
+    onError: (event) => {
+      const errorMessage = event.data?.zodError?.fieldErrors.content;
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0]);
+      } else {
+        toast.error('Failed to post! Please try again later');
+      }
     },
   });
 
   if (!user) return null;
- 
+
   return (
     <div className="flex w-full gap-3">
       <Image
@@ -35,12 +44,27 @@ const CreatePostWizard: FC = () => {
       <input
         placeholder="Type some text"
         className="grow bg-transparent outline-none"
-        type='text'
+        type="text"
         value={input}
-        onChange={(e) => setInput(e.target.value)}
+        onChange={(event) => setInput(event.target.value)}
+        onKeyDown={(event) => {
+          if (event.key === 'Enter') {
+            event.preventDefault();
+            if (input !== '') {
+              mutate({ content: input });
+            }
+          }
+        }}
         disabled={isPosting}
       />
-      <button onClick={() => mutate({content: input})}>Post</button>
+      {input !== '' && !isPosting && (
+        <button onClick={() => mutate({ content: input })}>Post</button>
+      )}
+      {isPosting && (
+        <div className="flex items-center justify-center">
+          <LoadingSpinner size={20} />
+        </div>
+      )}
     </div>
   );
 };
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,8 +1,12 @@
-import type { User } from "@clerk/nextjs/dist/api";
-import { clerkClient } from "@clerk/nextjs/server";
-import { TRPCError } from "@trpc/server";
-import { z } from "zod";
-import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
+import type { User } from '@clerk/nextjs/dist/api';
+import { clerkClient } from '@clerk/nextjs/server';
+import { TRPCError } from '@trpc/server';
+import { z } from 'zod';
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from '~/server/api/trpc';
 
 // publicProcedure - a method to generate the function that the client calls
 // stuff returned will be available to all users
@@ -14,12 +18,12 @@ const filterUserForClient = (user: User) => {
   };
 };
 
-import { Ratelimit } from "@upstash/ratelimit"; 
-import { Redis } from "@upstash/redis";
+import { Ratelimit } from '@upstash/ratelimit';
+import { Redis } from '@upstash/redis';
 
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  limiter: Ratelimit.slidingWindow(3, '1 m'),
   analytics: true,
 });
 
@@ -27,9 +31,11 @@ export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.prisma.post.findMany({
       take: 100,
-      orderBy: [{
-        createdAt: "desc" 
-      }]
+      orderBy: [
+        {
+          createdAt: 'desc',
+        },
+      ],
     });
 
     const users = (
@@ -44,34 +50,36 @@ export const postsRouter = createTRPCRouter({
 
       if (!author)
         throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "Author for post not found",
+          code: 'INTERNAL_SERVER_ERROR',
+          message: 'Author for post not found',
         });
 
       return {
         post,
         author: {
           ...author,
-          username: "MyUser",
+          username: 'MyUser',
         },
       };
     });
   }),
 
-  create: privateProcedure.input(z.object({content: z.string()})).mutation(async ({ctx, input}) => {
-    const authorId = ctx.userId; 
-  
-    const {success} = await ratelimit.limit(authorId);
+  create: privateProcedure
+    .input(z.object({ content: z.string().max(15) }))
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.userId;
 
-    if (!success) throw new TRPCError({code: "TOO_MANY_REQUESTS"})
-    
-    const post = await ctx.prisma.post.create({
-      data: {
-        authorId,
-        content: input.content,
-      }
-    })
+      const { success } = await ratelimit.limit(authorId);
 
-    return post;
-  })
+      if (!success) throw new TRPCError({ code: 'TOO_MANY_REQUESTS' });
+
+      const post = await ctx.prisma.post.create({
+        data: {
+          authorId,
+          content: input.content,
+        },
+      });
+
+      return post;
+    }),
 });
